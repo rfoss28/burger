a2f039d1f494f86ee0ec845c65a62de0fc26eb2c
Ryan <rfoss28@comcast.net>
2018-09-30 05:27:46 -0400

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-
+//test
 
 app.use("/", routes);
 
